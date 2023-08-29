1fe8b0125d26c2ec26aae6d7d0e42052f5c8fbdb
Abel Pierna <103205401+Abelino537@users.noreply.github.com>
2023-08-28 21:52:37 -0300

    Clase 2
    Agrego la clase 2 de node

---
+++ Clase2/app.js
@@ -0,0 +1,17 @@
+/*
+UTN
+Tec. Programación
+Metodologia de Sistemas
+Node
+23/08/23
+Abel Pierna
+Clase 2
+*/
+
+const nombre = 'Abel';
+console.log(nombre);
+
+const nombre2 = 'Jere';
+console.log(nombre2);
+
+console.log('Hola mundo desde node')
